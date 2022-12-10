433e889ec4c9ce5a8c609da079442147c53e7844
dailx-1056 <luu.xuan.dai@sun-asterisk.com>
2022-12-10 22:51:34 +0700

    delete and edit post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,18 +1,18 @@
 class Admin::PostsController < ApplicationController
+  before_action :load_post, only: %i[show edit update destroy]
+
   def index
     @pagy, @posts = pagy(Post.includes(:author))
   end
 
-  def show
-    @post = Post.find params[:id]
-  end
+  def show; end
 
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new create_post_params
+    @post = Post.new post_params
 
     if @post.save
       redirect_to admin_posts_path
@@ -21,9 +21,31 @@ class Admin::PostsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to admin_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to admin_posts_path
+    else
+      # Message here
+    end
+  end
+
   private
 
-  def create_post_params
+  def load_post
+    @post = Post.find params[:id]
+  end
+
+  def post_params
     params.require(:post).permit(:subject, :content)
   end
 end
