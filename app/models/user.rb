9fe38fbb802c2ad472f827f6386c20c415db912a
root <jojolove198@gmail.com>
2022-12-18 22:11:17 +0000

    quick fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :validatable, :lockable, :trackable
+  devise :database_authenticatable
   USER_WHITELIST_ATTRIBUTES = %i[name email password password_confirmation].freeze
 
   validates :name, :email, presence: true
