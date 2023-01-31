885a2335b52c1ccde7bcf0697abfaa792c616e57
root <jojolove198@gmail.com>
2023-01-31 16:54:50 +0000

    fix cron enviroment

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,8 @@
-env :PATH, ENV['PATH']
+ENV.each_key do |key|
+  env key.to_sym, ENV[key]
+end
+
+set :enviroment, ENV["RAILS_ENV"]
 
 every 1.day, at: '00:00 am' do
   runner 'GenerateReminderJob.perform_async'
