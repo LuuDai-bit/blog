d78638f940ab7960e71adcb43e5e723a715468bf
LuuDai-bit <jojolove198@gmail.com>
2024-02-01 10:29:20 +0700

    User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:user_reminders) }
+    it { should have_many(:reminders) }
+  end
 end
