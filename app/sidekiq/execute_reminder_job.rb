ac7c70c7e5667862b07b596ba972cd6e15ce14c1
LuuDai-bit <dailx1912@gmail.com>
2026-01-03 16:33:57 +0700

    Remove unused file and fix some logic bug

--- app/sidekiq/execute_reminder_job.rb
+++ app/sidekiq/execute_reminder_job.rb
@@ -5,7 +5,7 @@ class ExecuteReminderJob
     reminder = Reminder.find(reminder_id)
     user = User.find(user_id)
 
-    notification_service = Factories::Notification::ReminderCreator.new(reminder.notification_type).create
+    notification_service = Factories::Notification::ReminderCreator.new(user, reminder.notification_type).create
     notification_service.send_notification(destination, reminder.title, reminder.content)
 
     reminder.destroy! if reminder.only_once
