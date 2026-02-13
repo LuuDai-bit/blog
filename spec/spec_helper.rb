bc978626445aa0a0ee98197a98bb1c689dda9c37
LuuDai-bit <dailx1912@gmail.com>
2026-02-13 09:41:26 +0700

    Trigger

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'simplecov-lcov'
 SimpleCov::Formatter::LcovFormatter.config do |c|
   c.report_with_single_file = true
   c.output_directory = 'coverage'
+  c.lcov_file_name = 'lcov.info'
 end
 
 SimpleCov.formatters = [
