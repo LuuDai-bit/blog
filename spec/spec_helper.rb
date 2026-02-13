cea731f9f5fd2340bc574b7484a4f046eb666962
LuuDai-bit <dailx1912@gmail.com>
2026-02-13 09:16:19 +0700

    Set up codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
-require "simplecov"
+require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.output_directory = "coverage"
+end
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+]
 
 SimpleCov.start "rails" do
   enable_coverage :branch
