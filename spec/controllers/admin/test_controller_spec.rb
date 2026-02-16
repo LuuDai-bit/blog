3fac816947f23f8f48dbf1132b2393464b61acf9
LuuDai-bit <dailx1912@gmail.com>
2026-02-16 08:07:38 +0700

    Test codecov message

--- spec/controllers/admin/test_controller_spec.rb
+++ spec/controllers/admin/test_controller_spec.rb
@@ -2,20 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Admin::TestController, type: :controller do
   describe 'GET #index' do
-    let!(:user) { create(:user) }
-
-    subject { get :index }
-
-    before { sign_in(user) }
-
-    context 'when success' do
-      it 'should return users' do
-        subject
-
-        users = assigns(:users)
-
-        expect(users.pluck(:id)).to eq [user.id]
-      end
-    end
   end
 end
