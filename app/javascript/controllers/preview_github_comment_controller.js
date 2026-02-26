3a21c706cbce167111f75f061f05032ec0f33597
LuuDai-bit <dailx1912@gmail.com>
2026-02-26 14:35:38 +0700

    Preview wont load when enter update page for the first time

--- app/javascript/controllers/preview_github_comment_controller.js
+++ app/javascript/controllers/preview_github_comment_controller.js
@@ -5,7 +5,12 @@ export default class extends Controller {
   static targets = [ "source", "preview" ]
 
   connect() {
-    generatePreview();
+    const inputValue = this.sourceTarget.value;
+    const reader   = new Parser();
+    const writer   = new HtmlRenderer();
+    const parsed   = reader.parse(inputValue);
+    const html     = writer.render(parsed);
+    this.previewTarget.innerHTML = html;
   }
 
   generatePreview() {
