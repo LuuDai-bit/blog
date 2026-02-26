67358ec2d71de8967526d90cc99e9801d0c89183
LuuDai-bit <dailx1912@gmail.com>
2026-02-26 09:55:30 +0700

    Fix 500 when update fail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def display_error object, method, name
+  def display_error(object, method, name)
     return unless object&.errors.present? && object.errors.key?(method)
 
     error = "#{name} #{object.errors.messages[method][0]}"
