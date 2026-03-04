a0384c700fb5a7fe340d8f92374f85ffc16bd1d0
LuuDai-bit <dailx1912@gmail.com>
2026-03-04 09:42:49 +0700

    Get variables and view repository variable in comment template

--- app/javascript/controllers/preview_github_comment_controller.js
+++ app/javascript/controllers/preview_github_comment_controller.js
@@ -2,23 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 import { Parser, HtmlRenderer } from "commonmark";
 
 export default class extends Controller {
-  static targets = [ "source", "preview" ]
+  static targets = ["source", "preview", "variablesPath", "variables", "repositoriesSelect"]
 
   connect() {
-    const inputValue = this.sourceTarget.value;
-    const reader   = new Parser();
-    const writer   = new HtmlRenderer();
-    const parsed   = reader.parse(inputValue);
-    const html     = writer.render(parsed);
-    this.previewTarget.innerHTML = html;
+    this.generatePreview();
   }
 
   generatePreview() {
     const inputValue = this.sourceTarget.value;
-    const reader   = new Parser();
-    const writer   = new HtmlRenderer();
-    const parsed   = reader.parse(inputValue);
-    const html     = writer.render(parsed);
+    const reader = new Parser();
+    const writer = new HtmlRenderer();
+    const parsed = reader.parse(inputValue);
+    const html = writer.render(parsed);
     this.previewTarget.innerHTML = html;
   }
+
+  reloadVariables() {
+    const variablesPath = this.variablesPathTarget.value;
+    const repositoryId = this.repositoriesSelectTarget.value;
+    const fetchVariables = async () => {
+      const response = await fetch(`${variablesPath}?repository_id=${repositoryId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      })
+      const data = await response.json();
+      console.log('debug')
+      console.log(data.data);
+      this.variablesTarget.innerHTML = data.data;
+    };
+
+    fetchVariables();
+  }
+
 }
