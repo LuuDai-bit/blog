8615f932690007cd4db325bd9afd016c4c737f63
LuuDai-bit <dailx1912@gmail.com>
2026-03-24 09:44:37 +0700

    Config

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,14 +1,11 @@
 Datadog.configure do |c|
-  c.service = 'my-rails-app'   # your app name
+  c.service = 'blog'
   c.env = Rails.env
   c.version = '1.0.0'
 
-  # Enable Rails tracing
   c.tracing.instrument :rails
 
-  # Optional: ActiveRecord
   c.tracing.instrument :active_record
 
-  # Optional: Redis
   c.tracing.instrument :redis
 end
