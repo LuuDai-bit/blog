359f62e5ddacd9aecaa0c57be1bf7c0b81c07dbb
LuuDai-bit <dailx1912@gmail.com>
2026-05-18 09:37:25 +0700

    Add log

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -8,11 +8,13 @@ module ErrorHandler
 
   private
 
-  def internal_server_error_message
+  def internal_server_error_message(error)
+    Rails.logger.info("Internal server error: #{error.message}")
     render json: { message: 'Something went wrong. Please check the log or contact admin' }, status: :internal_server_error
   end
 
-  def record_not_found_message
+  def record_not_found_message(error)
+    Rails.logger.info("Record not found: #{error.message}")
     render json: { message: 'Record not found' }, status: :not_found
   end
 end
