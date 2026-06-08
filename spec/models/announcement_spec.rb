0f1826c275de9f3f196b888f8bfeeff6c74dfcfe
LuuDai-bit <dailx1912@gmail.com>
2026-06-08 09:45:58 +0700

    Add unit test for announcement model

--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -14,4 +14,24 @@ RSpec.describe Announcement, type: :model do
     it { should validate_presence_of(:color_config) }
     it { should validate_inclusion_of(:color_config).in_array(Dialog.allowed_options) }
   end
+
+  describe 'instance methods' do
+    describe 'css_class' do
+      context 'when type is default' do
+        it 'should return css class based on type' do
+          announcement.color_config = :default
+          css_class = announcement.css_class
+          expect(css_class).to eq('dialog-default')
+        end
+      end
+
+      context 'when type is rainbow' do
+        it 'should return rainbow css class' do
+          announcement.color_config = :rainbow
+          css_class = announcement.css_class
+          expect(css_class).to eq('dialog-rainbow')
+        end
+      end
+    end
+  end
 end
