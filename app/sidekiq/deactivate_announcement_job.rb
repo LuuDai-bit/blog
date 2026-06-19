9b0a8bc971b466bd155fced4eb2daab337160807
LuuDai-bit <dailx1912@gmail.com>
2026-06-19 07:41:34 +0700

    Change divider character

--- app/sidekiq/deactivate_announcement_job.rb
+++ app/sidekiq/deactivate_announcement_job.rb
@@ -14,7 +14,7 @@ class DeactivateAnnouncementJob
       current_time: Time.current
     }
     json_string_params = JSON.generate(params)
-    params_string = "deactivate_announcement_job:#{json_string_params}"
+    params_string = "deactivate_announcement_job^-#{json_string_params}"
     RedisModel::Event.create(queue: 'default_jobs', params: params_string)
   end
 end
