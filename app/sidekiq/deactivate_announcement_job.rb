f7c3205861ffcba8673f276d1f4e51447c407195
LuuDai-bit <dailx1912@gmail.com>
2026-06-22 09:52:11 +0700

    Remove deprecated deactivate announcement code

--- app/sidekiq/deactivate_announcement_job.rb
+++ app/sidekiq/deactivate_announcement_job.rb
@@ -3,10 +3,6 @@ class DeactivateAnnouncementJob
   include Sidekiq::Job
 
   def perform
-    # TODO: Deprecated. Will remove before the sidekiq service terminated
-    announcements = Announcement.active.where('end_at < ?', Time.current)
-    announcements.update_all(activated: false)
-
     JobLog.create(job_name: self.class.name)
 
     # Newly job service. Testing
