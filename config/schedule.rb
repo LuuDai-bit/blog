f5fa2d73bb2b2a18ffb032e28faf99ca5cd12b36
LuuDai-bit <dailx1912@gmail.com>
2026-07-07 14:53:05 +0700

    Change schedule file

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,14 +4,15 @@ end
 
 set :enviroment, ENV["RAILS_ENV"]
 
+# TODO: Will have a separate container for cron
 every 1.day, at: '00:00 am' do
   runner 'GenerateReminderJob.perform_async'
 end
 
 every 1.day, at: '01:00 am' do
-  runner 'SyncViewsToPostJob.perform_async'
+  runner 'SyncViewsToPostJob.new.perform'
 end
 
-# every 1.hour do
-#   runner 'DeactivateAnnouncementJob.perform_async'
-# end
+every 1.hour do
+  runner 'DeactivateAnnouncementJob.new.perform'
+end
