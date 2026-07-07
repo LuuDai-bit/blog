c53ee175778db7ed88cb0f37f032a847ae360b11
LuuDai-bit <dailx1912@gmail.com>
2026-07-07 10:21:41 +0700

    Sync view to job post

--- app/sidekiq/sync_views_to_post_job.rb
+++ app/sidekiq/sync_views_to_post_job.rb
@@ -4,28 +4,27 @@ class SyncViewsToPostJob
 
   def perform
     # TODO: Deprecated. Will remove before the sidekiq service terminated
-    view_hash = redis.client.hgetall(redis.post_views)
+    # view_hash = redis.client.hgetall(redis.post_views)
 
-    update_attr = []
-    Post.where(id: view_hash.keys).find_in_batches do |posts|
-      posts.each do |post|
-        post.views += view_hash[post.id.to_s].to_i
-        update_attr << post.attributes
-      end
+    # update_attr = []
+    # Post.where(id: view_hash.keys).find_in_batches do |posts|
+    #   posts.each do |post|
+    #     post.views += view_hash[post.id.to_s].to_i
+    #     update_attr << post.attributes
+    #   end
 
-      Post.upsert_all(update_attr)
-    end
+    #   Post.upsert_all(update_attr)
+    # end
 
-    redis.client.del(redis.post_views)
+    # redis.client.del(redis.post_views)
 
     JobLog.create(job_name: self.class.name)
 
     # Newly job service. Testing
-    params = {
-      name: "sync_views_to_post",
-      post_views_key: redis.post_views
-    }
-    RedisModel::Event.create(queue: 'default_jobs', params: params)
+    params = {}
+    json_string_params = JSON.generate(params)
+    params_string = "sync_views_to_post_job^-#{json_string_params}"
+    RedisModel::Event.create(queue: 'default_jobs', params: params_string)
   end
 
   private
