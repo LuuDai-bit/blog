ef0470b830130bdd826f880da9802660081a11e4
LuuDai-bit <dailx1912@gmail.com>
2026-07-08 08:19:50 +0700

    Clean up redundant code

--- app/sidekiq/sync_views_to_post_job.rb
+++ app/sidekiq/sync_views_to_post_job.rb
@@ -1,23 +1,5 @@
 class SyncViewsToPostJob
-  # TODO: Deprecated. Will remove before the sidekiq service terminated
-  include Sidekiq::Job
-
   def perform
-    # TODO: Deprecated. Will remove before the sidekiq service terminated
-    # view_hash = redis.client.hgetall(redis.post_views)
-
-    # update_attr = []
-    # Post.where(id: view_hash.keys).find_in_batches do |posts|
-    #   posts.each do |post|
-    #     post.views += view_hash[post.id.to_s].to_i
-    #     update_attr << post.attributes
-    #   end
-
-    #   Post.upsert_all(update_attr)
-    # end
-
-    # redis.client.del(redis.post_views)
-
     JobLog.create(job_name: self.class.name)
 
     # Newly job service. Testing
