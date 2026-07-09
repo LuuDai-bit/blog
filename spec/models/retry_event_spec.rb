81ba6d879b8aa40cc9416e2c49eac8eb06e790b7
LuuDai-bit <dailx1912@gmail.com>
2026-07-09 10:16:54 +0700

    Fix bug and update rspec

--- spec/models/retry_event_spec.rb
+++ spec/models/retry_event_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe RetryEvent, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:event_id) }
-    it { should validate_numericality_of(:retry_count).is_greater_than(0).is_less_than(Settings.models.retry_event.max_retry) }
+    it { should validate_numericality_of(:retry_count).is_greater_than(0).is_less_than_or_equal_to(Settings.models.retry_event.max_retry) }
   end
 end
