de932dd1386a3ca830db96f02cca2bb17aaf8cb6
LuuDai-bit <dailx1912@gmail.com>
2026-07-09 10:21:41 +0700

    Authentication for internal s2s api call

--- app/controllers/api/internal/application_controller.rb
+++ app/controllers/api/internal/application_controller.rb
@@ -1,8 +1,29 @@
 module Api
   module Internal
     class ApplicationController < ApplicationController
-      # Implement authorization
+      class AuthenticationError < StandardError; end
+
       include ErrorHandler
+
+      rescue_from AuthenticationError, with: :authentication_error
+
+      before_action :authenticate
+
+      private
+
+      def authenticate
+        auth_token = AuthToken.find_by(token: token)
+
+        raise AuthenticationError if auth_token.blank?
+      end
+
+      def token
+        request.headers['Token'] || request.headers['token']
+      end
+
+      def authentication_error
+        render json: { message: 'Can not authenticate' }, status: :unauthorized
+      end
     end
   end
 end
